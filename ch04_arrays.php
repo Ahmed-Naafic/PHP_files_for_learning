0e53e0fb53adbc546b7cf41573590979e926ee83
Ahmed-Naafic <ahmednornafic@gmail.com>
2025-11-09 18:55:39 +0300

    Declaring arrays

--- ch04_arrays.php
+++ ch04_arrays.php
@@ -10,7 +10,7 @@
     <?php
     // declare array
     $array=array("ahmed",25,"Jungale", true);
-    echo "$array[0]";
+    echo "$array[0]";    
 
     // display array details using var_dump()
       echo "<pre>";
